6d79cacc3979f7c45ae5781220a1a08e05d73859
bubsworth <bubbard96@hotmail.com>
2024-06-20 15:44:01 +0100

    Add footer

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,19 +5,24 @@ import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import ContactUs from '../pages/ContactUs/ContactUs';
 import FAQs from '../pages/FAQs/FAQs';
+import Footer from '../common/Footer/Footer';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/faqs" element={<FAQs />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>    
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/faqs" element={<FAQs />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
+
   );
 };
 
