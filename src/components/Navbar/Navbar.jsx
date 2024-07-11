b30632d6647656433ac97bf52efa6430d3686604
dshaw-yozu <dshaw@yozu.co.uk>
2024-07-11 10:19:10 +0100

    Moved existing inline svg into assets/svg folder

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,31 @@
-import { Link } from "react-router-dom";
-import DownloadButton from "../Download/DownloadButton";
-import Logo from "../../assets/logo.png"
+import { Link } from 'react-router-dom';
+import DownloadButton from '../Download/DownloadButton';
+import Logo from '../../assets/logo.png';
 
-const Navbar = () => {  
+import { ReactComponent as MenuClosedIcon } from '../../assets/svg/menu-closed.svg';
+
+const Navbar = () => {
   return (
     <>
       <nav className="inset-x-0 top-0 mx-auto flex max-w-7xl p-6 lg:px-8">
         <div className="flex-row items-center lg:flex lg:gap-x-12 lg:flex-1 uppercase">
-          <Link className="text-xl font-semibold leading-6 text-gray-900" to="/">Home</Link>
-          <Link className="text-xl font-semibold leading-6 text-gray-900" to="/about">About</Link>
+          <Link className="text-xl font-semibold leading-6 text-gray-900" to="/">
+            Home
+          </Link>
+          <Link className="text-xl font-semibold leading-6 text-gray-900" to="/about">
+            About
+          </Link>
         </div>
-        <div className="flex-1"><img src={Logo} className="h-16" alt="logo" /></div>
+        <div className="flex-1">
+          <img src={Logo} className="h-16" alt="logo" />
+        </div>
         <div className="flex lg:hidden">
-          <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
             <span className="sr-only">Open main menu</span>
-            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-              <path strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
+            <MenuClosedIcon />
           </button>
         </div>
         <DownloadButton />
